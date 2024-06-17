e3d060dcd6483d1e52b6f4fa9138f3afafc54ec8
Gabtaca <gabrieltaca117@gmail.com>
2024-06-17 11:53:02 -0400

    6

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,31 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import { fileURLToPath, URL } from 'url';
 
-// Get the directory name of the current module file
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  // No need to change the root if the root is the project directory
-  // Ensure the server is set up correctly
+
+  rollupOptions: {
+    input: 'src/index.html'
+  },
   server: {
     port: 8070,
     hot: true,
     base: '/',
-    // Additional configuration
     build: {
       outDir: 'dist',
   },
-  // Resolve aliases for cleaner imports, if needed
   resolve: {
     alias: {
-      // Example: '@' can be used to import from the `src` directory
       '@': path.resolve(__dirname, 'src')
-    }
+    },
+      scripts: {
+    "dev": "vite",
+    "build": "vite build",
+    "serve": "vite preview"
   }
+  }
+
   }
 });
